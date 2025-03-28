db5da4e8298694b8e4756c2aa65389bd8c814d69
TanniruHemaVarshini <hemavarshinitanniru@gmail.com>
2025-03-28 13:47:41 +0530

    Fixed deletion issue

--- mssproject/adminapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Admin,Music,Podcasts,Song
-# Register your models here.
-admin.site.register(Admin)
-admin.site.register(Music)
-admin.site.register(Podcasts)
-admin.site.register(Song)
